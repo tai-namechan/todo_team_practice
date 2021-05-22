9cc27f4631d3eb498dfe88e8c42f3e02b8cd873d
Taisei Namekawa <s.namety.0106.hta1@gmail.com>
2021-05-22 16:33:53 +0900

    Heroku deploy commit

--- dbconnect.php
+++ dbconnect.php
@@ -7,12 +7,22 @@ class DbManager
     public function connect()
     {
         //DBに接続
-        $host = "localhost";
+        // $host = "localhost";
         // $dbname = "データベース名";
-        $dbname = "todo_team";
-        $charset = "utf8mb4";
-        $user = 'root';
-        $password = '';
+        // $dbname = "todo_team";
+        // $charset = "utf8mb4";
+        // $user = 'root';
+        // $password = '';
+
+        // 本番環境で接続するための設定
+        $host = getenv('host'); //MySQLがインストールされてるコンピュータ
+        $dbname = getenv('dbname'); //使用するDB
+        $charset = "utf8"; //文字コード
+        $user = getenv('username'); //MySQLにログインするユーザー名
+        $password = getenv('password'); //ユーザーのパスワード
+
+
+
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
